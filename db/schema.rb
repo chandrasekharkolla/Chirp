450b5111df6d54f6603aadeb9cb560e8b58251b3
chandrasekharkolla <chandrasekharkolla@gmail.com>
2016-02-25 03:04:00 -0500

    Add firstname lastname mobile number columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222235227) do
+ActiveRecord::Schema.define(version: 20160225075410) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "mobile_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
